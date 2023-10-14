225655319a68def79bbde3f980114513fc075e9a
Neuroglial <FuturePast1288@qq.com>
2023-10-14 18:27:23 +0800

    Added base PBR render and a example.

--- premake5.lua
+++ premake5.lua
@@ -5,130 +5,135 @@ workspace "Jasmine3D"
 	configurations 
 	{ 
 		"Debug", 
-        "Release",
-        "Dist"
-    }
-    
-outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
-
--- Include directories relative to root folder (solution directory)
-IncludeDir = {}
-IncludeDir["GLFW"] = "Jasmine/vendor/GLFW/include"
-IncludeDir["Glad"] = "Jasmine/vendor/Glad/include"
-IncludeDir["ImGui"] = "Jasmine/vendor/ImGui"
-IncludeDir["glm"] = "Jasmine/vendor/glm"
-
-include "Jasmine/vendor/GLFW"
-include "Jasmine/vendor/Glad"
-include "Jasmine/vendor/ImGui"
-
-project "Jasmine"
-    location "Jasmine"
-    kind "StaticLib"
-    language "C++"
-    cppdialect "C++17"
-    staticruntime "on"
-
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-
-    pchheader "JMpch.h"
-    pchsource "Jasmine/src/JMpch.cpp"
-
-	files 
-	{ 
-		"%{prj.name}/src/**.h", 
-		"%{prj.name}/src/**.c", 
-		"%{prj.name}/src/**.hpp", 
-		"%{prj.name}/src/**.cpp" 
-    }
-
-    includedirs
-	{
-		"%{prj.name}/src",
-        "%{prj.name}/vendor",
-        "%{IncludeDir.GLFW}",
-        "%{IncludeDir.Glad}",
-        "%{IncludeDir.glm}",
-        "%{IncludeDir.ImGui}"
-    }
-    
-    	links 
-	{ 
-        	"GLFW",
-        	"Glad",
-        	"ImGui",
-		"opengl32.lib"
+        	"Release",
+        	"Dist"
     	}
     
-	filter "system:windows"
-     	systemversion "latest"
+	outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+	-- Include directories relative to root folder (solution directory)
+	IncludeDir = {}
+	IncludeDir["GLFW"] = "Jasmine/vendor/GLFW/include"
+	IncludeDir["Glad"] = "Jasmine/vendor/Glad/include"
+	IncludeDir["ImGui"] = "Jasmine/vendor/ImGui"
+	IncludeDir["glm"] = "Jasmine/vendor/glm"
+
+	include "Jasmine/vendor/GLFW"
+	include "Jasmine/vendor/Glad"
+	include "Jasmine/vendor/ImGui"
+
+	--------------------------------------------------------------------
+	project "Jasmine"
+    		location "Jasmine"
+    		kind "StaticLib"
+    		language "C++"
+    		cppdialect "C++17"
+    		staticruntime "on"
+
+		targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    		objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    		pchheader "JMpch.h"
+    		pchsource "Jasmine/src/JMpch.cpp"
+
+		files 
+		{ 
+			"%{prj.name}/src/**.h", 
+			"%{prj.name}/src/**.c", 
+			"%{prj.name}/src/**.hpp", 
+			"%{prj.name}/src/**.cpp" 
+    		}
+
+    		includedirs
+		{
+			"%{prj.name}/src",
+        		"%{prj.name}/vendor",
+        		"%{IncludeDir.GLFW}",
+        		"%{IncludeDir.Glad}",
+        		"%{IncludeDir.glm}",
+        		"%{IncludeDir.ImGui}",
+        		"%{prj.name}/vendor/assimp/include",
+        		"%{prj.name}/vendor/stb/include"
+    		}
+    
+    		links 
+		{ 
+        		"GLFW",
+        		"Glad",
+        		"ImGui",
+			"opengl32.lib"
+    		}
+    
+		filter "system:windows"
+     		systemversion "latest"
         
-	defines 
-	{ 
-           "JM_PLATFORM_WINDOWS",
-           "JM_BUILD_DLL"
-	}
+		defines 
+		{ 
+           	"JM_PLATFORM_WINDOWS",
+           	"JM_BUILD_DLL"
+		}
 
-    filter "configurations:Debug"
-        	defines "JM_DEBUG"
-        	symbols "On"
+    		filter "configurations:Debug"
+        		defines "JM_DEBUG"
+        		symbols "On"
                 
-    filter "configurations:Release"
-        	defines "JM_RELEASE"
-        	optimize "On"
+    		filter "configurations:Release"
+        		defines "JM_RELEASE"
+        		optimize "On"
 
-    filter "configurations:Dist"
-        	defines "JM_DIST"
-        	optimize "On"
+    		filter "configurations:Dist"
+        		defines "JM_DIST"
+        		optimize "On"
 
-project "Sandbox"
-    location "Sandbox"
-    kind "ConsoleApp"
-    language "C++"
-    cppdialect "C++17"
-    staticruntime "on"
+	--------------------------------------------------------------------
+	project "Sandbox"
+    		location "Sandbox"
+    		kind "ConsoleApp"
+    		language "C++"
+    		cppdialect "C++17"
+    		staticruntime "on"
     
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+		targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    		objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-	links 
-	{ 
-		"Jasmine"
-    	}
+		links 
+		{ 
+			"Jasmine",
+			"Jasmine/vendor/assimp/win64/assimp.lib"
+    		}
     
-	files 
-	{ 
-		"%{prj.name}/src/**.h", 
-		"%{prj.name}/src/**.c", 
-		"%{prj.name}/src/**.hpp", 
-		"%{prj.name}/src/**.cpp" 
-	}
+		files 
+		{ 
+			"%{prj.name}/src/**.h", 
+			"%{prj.name}/src/**.c", 
+			"%{prj.name}/src/**.hpp", 
+			"%{prj.name}/src/**.cpp" 
+		}
     
-	includedirs 
-	{
-        "%{prj.name}/src",
-        "Jasmine/src",
-        "Jasmine/vendor",
-        "%{IncludeDir.glm}"
-    	}
+		includedirs 
+		{
+        		"%{prj.name}/src",
+        		"Jasmine/src",
+        		"Jasmine/vendor",
+        		"%{IncludeDir.glm}"
+    		}
 	
-	filter "system:windows"
-        systemversion "latest"
+		filter "system:windows"
+        		systemversion "latest"
                 
 		defines 
 		{ 
-            "JM_PLATFORM_WINDOWS"
+            	"JM_PLATFORM_WINDOWS"
 		}
     
-    filter "configurations:Debug"
-        defines "JM_DEBUG"
-        symbols "on"
+    		filter "configurations:Debug"
+        		defines "JM_DEBUG"
+        		symbols "on"
                 
-    filter "configurations:Release"
-        defines "JM_RELEASE"
-        optimize "on"
+    		filter "configurations:Release"
+        		defines "JM_RELEASE"
+        		optimize "on"
 
-    filter "configurations:Dist"
-        defines "JM_DIST"
-        optimize "on"
+    		filter "configurations:Dist"
+        		defines "JM_DIST"
+        		optimize "on"
